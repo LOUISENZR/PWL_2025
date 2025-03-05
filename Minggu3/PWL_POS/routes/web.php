63c8d155a7c53234dbb0806551271aed9971eb98
Louise Nazarossa <louisanaza@gmail.com>
2025-03-06 02:21:04 +0700

    Flowquent

--- Minggu3/PWL_POS/routes/web.php
+++ Minggu3/PWL_POS/routes/web.php
@@ -1,29 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
-use Illuminate\Http\Request;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-class KategoriController extends Controller
-{
-    public function index()
-    {
-        //$data = [
-           // 'kategori_kode' => 'SNK',
-            //'kategori_nama' => 'Snack/Makanan Ringan',
-            //'created_at' => now()
-        //];
-        //DB::table('m_kategori')->insert($data);
-        //return 'Insert data baru berhasil';
-
-        //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        //return 'Update data berhasil. Jumlah data yang diupdate: ' .$row.' baris';
-
-        //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        //return 'Update data dihapus. Jumlah data yang dihapus: ' .$row.' baris';
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
-    }
-}
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
