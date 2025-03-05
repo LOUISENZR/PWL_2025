8fbcee3c7b4f11369daa27b4bbf0b44965b96c66
Louise Nazarossa <louisanaza@gmail.com>
2025-03-06 02:13:16 +0700

    QBuildier

--- Minggu3/PWL_POS/app/Http/Controllers/KategoriController.php
+++ Minggu3/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -9,11 +9,11 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
+        //$data = [
+            //'kategori_kode' => 'SNK',
+            //'kategori_nama' => 'Snack/Makanan Ringan',
+            //'created_at' => now()
+        //];
         //DB::table('m_kategori')->insert($data);
         //return 'Insert data baru berhasil';
 
