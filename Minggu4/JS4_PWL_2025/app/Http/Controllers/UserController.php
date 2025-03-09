cb3cee632034e8bd64cd8b0f0e26cdb1145a1e20
Louise Nazarossa <louisanaza@gmail.com>
2025-03-09 23:09:39 +0700

    Fillable

--- Minggu4/JS4_PWL_2025/app/Http/Controllers/UserController.php
+++ Minggu4/JS4_PWL_2025/app/Http/Controllers/UserController.php
@@ -10,14 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
+        // tambah data user dengan Fillable
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::insert($data); //tambahkan data ke m_user
+        UserModel::create($data); //membuat data baru
 
         //coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
