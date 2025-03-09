306a58879cd93977a4c5b338b7a1a9079eff2998
Louise Nazarossa <louisanaza@gmail.com>
2025-03-09 22:48:52 +0700

    copy

---
+++ Minggu4/JS4_PWL_2025/app/Models/UserModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan model 
+    protected $primarykey = 'id_user'; //Mendefinisikan primary key dari tabel yang digunakan
+    /*
+    * The atributes that are mass assiggnable.
+    *
+    *@var array
+    */
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+}
