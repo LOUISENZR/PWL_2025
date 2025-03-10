a4b9b83b5a7d61a2c1c73c5fe39352b04356d462
Louise Nazarossa <louisanaza@gmail.com>
2025-03-10 14:21:19 +0700

    Retreiving Agregates

--- Minggu4/JS4_PWL_2025/app/Http/Controllers/UserController.php
+++ Minggu4/JS4_PWL_2025/app/Http/Controllers/UserController.php
@@ -10,7 +10,27 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOrFail(1);        
+        //$user = UserModel::find(1);
+        
+
+        //$user = UserModel::where('level_id', 1)->first();
+        
+        //$user = UserModel::firstWhere('level_id', 1);
+
+        //$user = UserModel::findOr(1, ['username', 'nama'], function() {
+        // abort(404);
+        //});
+        
+        //$user = UserModel::findOr(20, ['username', 'nama'], function() {
+        // aabort(404);
+        //});
+
+        //$user = UserModel::findOrFail(1);
+
+        //$user = UserModel::where('username', 'manager9')->firstOrFail();
+        
+        $user = UserModel::where('level_id', 2) ->count();
+        //dd($user); //eksekusi berhenti di dd() dan mengabaikan tampilan user.blade
         return view('user', ['data' => $user]);
 
     }
