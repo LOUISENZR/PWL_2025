fcf36f2b624a8d99d0c339ec4e5efa7962aac914
Louise Nazarossa <louisanaza@gmail.com>
2025-03-27 07:58:06 +0700

    tuugas

---
+++ Minggu6/JS6_PWL_2025/database/seeders/PenjualanSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class PenjualanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [];
+
+        for ($i = 1; $i <= 10; $i++) {
+            $data[] = [
+                'user_id' => rand(1, 3),
+                'pembeli' => 'Pembeli ' . $i, 
+                'penjualan_kode' => 'PNJ00' . $i,
+                'penjualan_tanggal' => Carbon::now()->subDays(rand(1, 30)), 
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+        DB::table('t_penjualan')->insert($data);
+    }
+}
