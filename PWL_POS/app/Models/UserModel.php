911a690a73d5d01b77cf6cc6fb5d0bb52a8b0326
Louise Nazarossa <louisanaza@gmail.com>
2025-04-17 07:29:59 +0700

    Add7

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -5,23 +5,41 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan model 
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
     /**
-     * The attribute that are mass assignable.
-     * 
-     * @var array
-    */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+     * Relasi ke tabel level
+     */
 
     public function level(): BelongsTo
     {
-        return $this->belongsTo(LevelModel::class,'level_id', 'level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+    //mengecek apakah user memiliki role tertentu
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+    //mendapatkan kode role
+    public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
