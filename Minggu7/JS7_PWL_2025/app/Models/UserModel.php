a16c4f8e46e813bba11a2cb546676a58b12bc4b2
Louise Nazarossa <louisanaza@gmail.com>
2025-05-04 21:48:23 +0700

    Tugas 4

--- Minggu7/JS7_PWL_2025/app/Models/UserModel.php
+++ Minggu7/JS7_PWL_2025/app/Models/UserModel.php
@@ -2,26 +2,37 @@
 
 namespace App\Models;
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi class authticatable
 
-
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan model 
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
-    /**
-     * The attribute that are mass assignable.
-     * 
-     * @var array
-    */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
+    
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'create_at', 'update_at'];
+    protected $hidden = ['password']; //jangan di tampilkan saat select
+    protected $casts = ['password'=> 'hashed']; //casting password agar otomatis di hash
+    
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class,'level_id', 'level_id');
+    }
+
+    public function getRoleName (){
+        return $this->level->level_nama;
+    }
+ 
+    public function hasRole ($role) : bool{
+        return $this->level->level_kode == $role;
+    }
+
+    public function getRole(){
+        return $this->level->level_kode;
     }
 }
